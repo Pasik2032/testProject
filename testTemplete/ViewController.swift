0124214a81b7e4c35e04b171d1ce1adea09d7d8e
Daniil Pasiletskii <daniil.pasiletskii@iss.digital>
2023-04-08 18:29:18 +0300

    Initial Commit

---
+++ testTemplete/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testTemplete
+//
+//  Created by Даниил Пасилецкий on 08.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
